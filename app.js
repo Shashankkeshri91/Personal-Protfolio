29454e30b112a2099fa460a4ef64247dae2cc4cd
Shashank Keshri <shashankkeshri91109@gmail.com>
2023-05-27 18:12:01 +0530

    Master

--- app.js
+++ app.js
@@ -2,13 +2,3 @@ var typed = new Typed('#element', {
   strings: ['Web Developer', 'Cricket Player','Cosmos Lover','Traveller.'],
   typeSpeed: 50,
 });
-
-function button(){
-  document.getElementById("Button").visited;
-
-}
-
-function button(){
-  document.getElementById("Button").
-  
-}
