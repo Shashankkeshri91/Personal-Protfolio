72e7d2744b62ed742ed2c7fc5eefdb30982bde7a
Shashank Keshri <shashankkeshri91109@gmail.com>
2023-05-30 20:50:16 +0530

    Master

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ var typed = new Typed('#element', {
   strings: ['Web Developer', 'Cricket Player','Cosmos Lover','Traveller.'],
   typeSpeed: 50,
 });
+
+// function sendEmail(){
+//   Email.send({
+//     Host : "smtp.gmail.com",
+//     Username : "shashankkeshri91109@gmail.com",
+//     Password : "4April_2002",
+//     To : "you@isp.com",
+//     From : document.getElementById("Email"),
+//     Subject : "New Contact Details For Enquiry",
+//     Body :"Name: " + document.getElementById("Name").value  
+//         + "<br> Email: " + document.getElementById("Email").value 
+//         +"<br> Phone No.: " + document.getElementById("Number").value 
+//         + "<br> Message: " + document.getElementById("Message").value
+// }).then(
+//   message => alert("Message sent Succesfully")
+// );
+// }
