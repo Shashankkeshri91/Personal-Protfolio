5be7968010b5d00d8b4d36bc8eeea27d4ad37c84
Shashank Keshri <shashankkeshri91109@gmail.com>
2023-05-31 00:32:43 +0530

    Master

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 var typed = new Typed('#element', {
-  strings: ['Web Developer', 'Cricket Player','Cosmos Lover','Traveller.'],
-  typeSpeed: 50,
+  strings: ['Traveller', 'Cricket Player','Cosmos Lover','Web Developer.'],
+  typeSpeed: 40
 });
 
-// function sendEmail(){
-//   Email.send({
-//     Host : "smtp.gmail.com",
-//     Username : "shashankkeshri91109@gmail.com",
-//     Password : "4April_2002",
-//     To : "you@isp.com",
-//     From : document.getElementById("Email"),
-//     Subject : "New Contact Details For Enquiry",
-//     Body :"Name: " + document.getElementById("Name").value  
-//         + "<br> Email: " + document.getElementById("Email").value 
-//         +"<br> Phone No.: " + document.getElementById("Number").value 
-//         + "<br> Message: " + document.getElementById("Message").value
-// }).then(
-//   message => alert("Message sent Succesfully")
-// );
-// }
+function sendEmail() {
+  // Get input values from the form
+  var name = document.getElementById("Name").value;
+  var email = document.getElementById("Email").value;
+  var number = document.getElementById("Number").value;
+  var message = document.getElementById("Message").value;
+
+  // Configure your email settings
+  Email.send({
+      SecureToken: "d1da592f-3e8c-44e5-9985-3b7cf489d2c1",
+      To: "shashankkeshri91109@gmail.com", // Replace with your email address
+      From: email,
+      Subject: "New Message from Contact Form",
+      Body: "Name: " + name + "<br>Email: " + email + "<br>Number: " + number + "<br>Message: " + message
+  }).then(function(response) {
+      if (response === "OK") {
+          alert("Email sent successfully!");
+      } else {
+          alert("Email failed to send. Please try again later.");
+      }
+  });
+}
+sendEmail()
